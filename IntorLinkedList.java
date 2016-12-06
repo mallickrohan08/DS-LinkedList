80c8668440fcda467249deef4efba272b54131ad
mallickrohan08 <mallickrohan08@gmail.com>
2016-12-06 21:49:43 +0530

    documentatoin added for IntorLinkedList.java

--- IntorLinkedList.java
+++ IntorLinkedList.java
@@ -1,3 +1,8 @@
+/**
+*@author: Rohan Mallick
+*Basic Intro to Linked List
+*/
+/* Create a Node having two fields for data and pointer to next node*/
 class Node {
 	int data;
 	Node next;
@@ -9,6 +14,7 @@ class Node {
 class IntorLinkedList {
 	
 	public static void main(String args[]) {
+		/*Head is the starting point of any linked list*/
 		Node head = null;
 		Node n1 = new Node(1);
 		Node n2 = new Node(2);
@@ -17,11 +23,14 @@ class IntorLinkedList {
 		n1.next = n2;
 		n2.next = n3;
 		
-		System.out.println(head);
+		/******************************************
+		Display the Basic structure of linked List on Console.
+		********************************************/
+		System.out.println(head); 			//Head will point to first node.
 		System.out.println("--------------------");
 		System.out.println(n1);
 		System.out.println(n1.data);
-		System.out.println(n1.next);
+		System.out.println(n1.next);		//Next to First node will point to second node n2.
 		System.out.println("--------------------");
 		System.out.println(n2);
 		System.out.println(n2.data);
