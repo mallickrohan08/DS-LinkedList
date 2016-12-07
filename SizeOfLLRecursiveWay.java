1b49d4bc061618fcd04039ff5149ae7d73ec85e6
mallickrohan08 <mallickrohan08@gmail.com>
2016-12-07 21:06:25 +0530

    Added recursive and iterative way to find size of LL

--- SizeOfLLRecursiveWay.java
+++ SizeOfLLRecursiveWay.java
@@ -3,36 +3,27 @@ class Node {
 	Node next;
 	Node(int d) {
 		data = d;
-		next = null;
 	}
 }
 class SizeOfLLRecursiveWay {
 	static Node head = null;
 	
-	public int getCount() {
-		int size = 0;
-		Node temp = head;
-		if(temp == null) {
-			System.out.println("List is Empty");
-			return size;
+	public int getCount(Node node) {
+		if(node == null) {
+			return 0;
 		}
-		while(temp != null) {
-			size++;
-			temp = temp.next;
-		}
-		return size;
+		return 1 + getCount(node.next);
 	}
 	
 	public static void main(String args[]) {
 		SizeOfLLRecursiveWay obj = new SizeOfLLRecursiveWay();
+		System.out.println(obj.getCount(head));
 		Node n1 = new Node(1);
 		Node n2 = new Node(2);
 		Node n3 = new Node(3);
 		head = n1;
 		n1.next = n2;
 		n2.next = n3;
-		System.out.println("Size : " + obj.getCount());
+		System.out.println(obj.getCount(head));
 	}
-	
-	
 }
---
+++ SizeOfLlIterativeWay.java
@@ -0,0 +1,37 @@
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+		data = d;
+		next = null;
+	}
+}
+class SizeOfLlIterativeWay {
+	static Node head = null;
+	
+	public int getCount() {
+		int size = 0;
+		Node temp = head;
+		if(temp == null) {
+			System.out.println("List is Empty");
+			return size;
+		}
+		while(temp != null) {
+			size++;
+			temp = temp.next;
+		}
+		return size;
+	}
+	
+	public static void main(String args[]) {
+		SizeOfLlIterativeWay obj = new SizeOfLlIterativeWay();
+		System.out.println("Size : " + obj.getCount());
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		head = n1;
+		n1.next = n2;
+		n2.next = n3;
+		System.out.println("Size : " + obj.getCount());
+	}	
+}
