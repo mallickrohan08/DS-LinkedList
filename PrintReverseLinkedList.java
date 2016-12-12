83bd1aa1897fced960359e3bc0359b37a4fb675d
mallickrohan08 <mallickrohan08@gmail.com>
2016-12-12 19:41:47 +0530

    Revese and Print linked List

---
+++ PrintReverseLinkedList.java
@@ -0,0 +1,23 @@
+/*
+  Insert Node at the end of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+
+void ReversePrint(Node head) {
+  // This is a "method-only" submission. 
+  // You only need to complete this method. 
+    Node temp = head;
+    if(temp == null) {
+        return;
+    }
+    ReversePrint(temp.next);
+    System.out.println(temp.data);
+
+}
